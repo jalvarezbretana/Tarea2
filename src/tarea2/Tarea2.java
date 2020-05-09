6fcc0f54b77145adaf9377ec01659262d00d85ad
jalvarezbretana <jalvarezbretana@danielcastelao.org>
2020-05-09 16:13:00 +0200

    Tarefa 2 rematada

---
+++ src/tarea2/Tarea2.java
@@ -0,0 +1,20 @@
+
+package tarea2;
+
+/**
+ *
+ * @author josel
+ */
+public class Tarea2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    Ventana ven = new Ventana();
+    ven.setVisible(true);
+    Ventana2 ven2 = new Ventana2();
+    ven2.setVisible(true);
+    }
+    
+}
